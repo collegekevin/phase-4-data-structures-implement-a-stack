e174b66b830ce88254ffa3db05a9a007fc267ee6
Kevin Price <collegekevin@gmail.com>
2023-02-01 11:39:53 -0800

    commit tree 5c47ca8b9686eacb7b1385639d9234efb87bbcb0

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
 # your code here
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        if @stack.size < limit
+            @stack.push(value)
+        else raise_error
+        end
+    end
+
+    def pop
+        @stack.pop
+    end 
+
+    def peek
+        @stack.last
+    end
+
+end
